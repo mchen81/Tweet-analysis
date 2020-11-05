1b784aa4a01d87b4e5d92b10c6a9c33dcaaba4f1
Jiachen Liu <jliu158@dons.usfca.edu.com>
2020-11-04 18:12:29 -0800

    add advanced result

--- word-count-main/src/main/java/util/WordSentimentUtil.java
+++ word-count-main/src/main/java/util/WordSentimentUtil.java
@@ -12,7 +12,7 @@ import java.util.StringTokenizer;
 public class WordSentimentUtil {
 
     public final static Set<String> positiveWordsSet = new HashSet<>();
-    public final static Set<String> negativeWordsSet = new HashSet<>();
+    public final static Set<String> negativeWordsSet = new HashSet<>(git);
 
     static {
 
